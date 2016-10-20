f3474ad5ad954c679ec6f5820ca10e808de8cc7a
th86 <th8623@gmail.com>
2016-10-20 10:12:42 -0400

    bugs cleaned
    Fixed the enumeration (range of for), condition in basisFunction, added
    value normalization.

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 Mutual Information between Continuous Variables in Python
 ====
 
-An implementation of [a B-Spline-based algorithm](https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-5-118). A nice R version is developed by [Weiyi Cheng](https://github.com/weiyi-bitw/cafr).
+This is an implementation of [a B-Spline-based algorithm](https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-5-118). There is a nice R version developed by [Weiyi Cheng](https://github.com/weiyi-bitw/cafr).
 
 ## Usage ##
 
 ```r
-import numpy as np
 import mutualinfo as m
-m.mi2D(np.linspace(0,1,10), np.linspace(0,1,10), 6, 3)
+m.mi2D([1,2,7], [0,0,1], 6, 3)
 ```
 
 ## Prerequisites ##
 
-Python2.7 and NumPy
+Python 2.7 and NumPy
 
 ## License ##
 
--- mutualinfo.py
+++ mutualinfo.py
@@ -10,13 +10,13 @@ import numpy as np
 #Cox-de Boor recursion formula
 def basisFunction( i, p, t, kVector, numBins):
   if p==1:
-    
-    if ((t>=kVector[i]) and (t< kVector[i+1]) and (kVector[i] < kVector[i-1])):
+    if ((t >= kVector[i]) and (t < kVector[i+1]) and (kVector[i] < kVector[i+1])): #knot[i]<t<knot[i+1], knot[i]<knot[i+1]
       return 1;
-    elif abs(t-kVector[i+1])<1e-10 and (i+1 == numBins):
+    elif (abs(t-kVector[i+1]) < 1e-10 and (i+1 == numBins)):
       return 1;
     else:
       return 0;
+
   else:
 
     d1 = kVector[i+p-1] - kVector[i]; #u(i+p)-ui
@@ -24,18 +24,18 @@ def basisFunction( i, p, t, kVector, numBins):
     d2 = kVector[i+p] - kVector[i+1]; #u(i+p+1)-u(i+1)
     n2 = kVector[i+p] - t;            #u(i+p+1)-u
     
-    if d1<1e-10 and d2<1e-10: 
-      e1=0;
-      e2=0;
-    elif d1<1e-10:
-      e1=0
-      e2=n2/d2*basisFunction(i+1, p-1, t, kVector, numBins); #N(i+1,p-1)(u)
-    elif d2<1e-10:
-      e1=n1/d1*basisFunction(i, p-1, t, kVector, numBins); #N(i,p-1)(u)
-      e2=0;
+    if d1 < 1e-10 and d2 < 1e-10: 
+      e1 = 0;
+      e2 = 0;
+    elif d1 < 1e-10:
+      e1 = 0;
+      e2 = n2/d2*basisFunction(i+1, p-1, t, kVector, numBins);  #N(i+1,p-1)(u)
+    elif d2 < 1e-10:
+      e1 = n1/d1*basisFunction(i, p-1, t, kVector, numBins);  #N(i,p-1)(u)
+      e2 = 0;
     else:
-      e1=n1/d1*basisFunction(i, p-1, t, kVector, numBins); #N(i,p-1)(u)
-      e2=n2/d2*basisFunction(i+1, p-1, t, kVector, numBins); #N(i+1,p-1)(u)
+      e1 = n1/d1*basisFunction(i, p-1, t, kVector, numBins);  #N(i,p-1)(u)
+      e2 = n2/d2*basisFunction(i+1, p-1, t, kVector, numBins);  #N(i+1,p-1)(u)
 
     if e1+e1<0 :
       return 0;
@@ -44,53 +44,61 @@ def basisFunction( i, p, t, kVector, numBins):
 
 #Convert the values into B-Spline weights
 def findWeights(x, knots, numBins, splineOrder): #only takes values within 0-1
-  numSamples = len(x)
-  weights = np.zeros(numSamples*numBins)
-  for curSample in range(0,(numSamples-1)):
-    for curBin in range(0,(numBins-1)):
-      weights[curBin*numSamples+curSample]=basisFunction(curBin, splineOrder, x[curSample], knots, numBins);
+  numSamples = len(x);
+  weights = np.zeros(numSamples*numBins);
+
+  xmax = max(x);
+  xmin = min(x);
+
+  for i in range(0, len(x)): #In python it means [0,len(x))
+  x[i] = (x[i]-xmin)/(xmax-xmin);
+
+  for i in range(0,numSamples):
+    for j in range(0,numBins):
+      weights[j*numSamples+i] = basisFunction(j, splineOrder, x[i], knots, numBins);
 
   return weights;
 
 #1-D Entropy
 def entropy1D(weights, numSamples, numBins ):
   H = 0;
-  for i in range(0, (numBins-1)): #compute the entropy of every bin using the B-spline-based probabilities.
+  for i in range(0,numBins): #compute the entropy of every bin using the B-spline-based probabilities.
     p = 0;
-    for j in range(0,(numSamples-1)): #go through every sample
-      p = p + weights[i*numSamples + j];
+    for j in range(0,numSamples): #go through every sample
+      p = p + weights[i*numSamples+j];
     if p>0:
-      H=H-(p/numSamples)*np.log2(p/numSamples);
+      H = H-(p/numSamples)*np.log2(p/numSamples);
 
   return H;
 
 #2-D Entropy 
 def entropy2D(weightsx, weightsy, numSamples, numBins):
   H = 0;
-  for ix in range(0, (numBins-1)):
-    for iy in range(0, (numBins-1)):
+  for ix in range(0, numBins):
+    for iy in range(0,numBins):
       p = 0;
-      for j in range(0,(numSamples-1)):
+      for j in range(0,numSamples):
         p = p + weightsx[ix*numSamples + j] * weightsy[iy*numSamples + j];
       if p>0:
-        H=H-(p/numSamples)*np.log2(p/numSamples);
+        H = H-(p/numSamples)*np.log2(p/numSamples);
 
   return H;
 
 #Mutual Information
 def mi2D(x,y,numBins,splineOrder):
   numSamples=len(x);
-  u=np.zeros(numBins+splineOrder);
-
-  #Put default weights in u, so that u[0:(bin+so-1)]= _____/-----
+  
+  #Put default weights in knots, so that knots[0:(bin+so-1)]= _____/-----
+  knots = np.zeros(numBins+splineOrder);
   nInternalPoints = numBins - splineOrder; 
-  for i in range(splineOrder, (splineOrder+nInternalPoints-1) ):
-    u[i]=(i-splineOrder+1)/(nInternalPoints+1);
-  for i in range(splineOrder+nInternalPoints, (numBins+splineOrder-1) ):
-    u[i]=1;
 
-  wx=findWeights(x, u, numBins, splineOrder);
-  wy=findWeights(y, u, numBins, splineOrder);
+  for i in range(splineOrder, (splineOrder+nInternalPoints) ):
+    knots[i] = (i-splineOrder+1)/(nInternalPoints+1);
+  for i in range(splineOrder+nInternalPoints, (numBins+splineOrder) ):
+    knots[i] = 1;
+ 
+  wx=findWeights(x, knots, numBins, splineOrder);
+  wy=findWeights(y, knots, numBins, splineOrder);
 
   mi= entropy1D(wx, numSamples, numBins ) + entropy1D(wy, numSamples, numBins ) - entropy2D(wx ,wy, numSamples, numBins);
 
